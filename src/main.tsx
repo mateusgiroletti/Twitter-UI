f0213a5bdb6c66bcc13d1b3eab48a024d3b867f1
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-02-12 16:15:09 -0300

    adding styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { House, Hash, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle } from "phosphor-react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./global.css";
+
+import logo from "./assets/logo.svg";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <div className="layout">
+            <aside className="sidebar">
+                <img className="logo" src={logo} alt="Logo" />
+
+                <nav className="main-navigation">
+                    <a href="" className="active">
+                        <House weight="fill" />
+                        Home
+                    </a>
+                    <a href="">
+                        <Hash />
+                        Explore
+                    </a>
+                    <a href="">
+                        <Bell />
+                        Notifications
+                    </a>
+                    <a href="">
+                        <Envelope />
+                        Messages
+                    </a>
+                    <a href="">
+                        <BookmarkSimple />
+                        Bookmarks
+                    </a>
+                    <a href="">
+                        <FileText />
+                        List
+                    </a>
+                    <a href="">
+                        <User />
+                        Profile
+                    </a>
+                    <a href="">
+                        <DotsThreeCircle />
+                        More
+                    </a>
+                </nav>
+
+                <button className="new-tweet" type="button">
+                    Tweet
+                </button>
+            </aside>
+            <div className="content">
+                content
+            </div>
+        </div>
     </React.StrictMode>,
-)
+);
