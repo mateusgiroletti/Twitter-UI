6ad0cac1e2c5d1e47de4f23e549d06fbf4f91688
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-02-20 18:52:24 -0300

    adding responsiveness

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { House, Hash, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle } from "phosphor-react";
+import { House, Hash, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle, Pencil } from "phosphor-react";
 import { NavLink } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
@@ -12,40 +12,41 @@ export function Sidebar() {
             <nav className="main-navigation">
                 <NavLink to="/" >
                     <House weight="fill" />
-                    Home
+                    <span>Home</span>
                 </NavLink>
                 <a href="">
                     <Hash />
-                    Explore
+                    <span>Explore</span>
                 </a>
                 <a href="">
                     <Bell />
-                    Notifications
+                    <span>Notifications</span>
                 </a>
                 <a href="">
                     <Envelope />
-                    Messages
+                    <span>Messages</span>
                 </a>
                 <a href="">
                     <BookmarkSimple />
-                    Bookmarks
+                    <span>Bookmarks</span>
                 </a>
                 <a href="">
                     <FileText />
-                    List
+                    <span>List</span>
                 </a>
                 <a href="">
                     <User />
-                    Profile
+                    <span>Profile</span>
                 </a>
                 <a href="">
                     <DotsThreeCircle />
-                    More
+                    <span>More</span>
                 </a>
             </nav>
 
             <button className="new-tweet" type="button">
-                Tweet
+                <Pencil />
+                <span>Tweet</span>
             </button>
         </aside>
     );
--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
 
+import "./index.css";
+
 export function Default() {
     return (
         <div className="layout">
